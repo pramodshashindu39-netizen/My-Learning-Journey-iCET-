d6a55f86682fe50f86db13c9d77e849bd95ec057
pramodshashindu39-netizen <pramodshashindu39@gmail.com>
2026-09-15 11:26:37 +0530

    console.log and Variables

---
+++ js/app.js
@@ -0,0 +1,26 @@
+console.log("Hii Again ");
+console.log("This is my first External JavaScript");
+
+//Variables 
+let name = "Shasindu Pramod";
+console.log(name);
+console.log(typeof name);
+
+let studentName = "Shasindu Pramod" ;
+let studentPass = true;
+let studentAge = [];
+let student = {};
+
+console.log(typeof studentName);
+console.log(typeof studentAge);
+console.log(typeof studentPass);
+console.log(typeof student);
+
+
+
+
+
+
+
+
+
