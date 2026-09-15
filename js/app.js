38c7059b9abda18e85c9f9e3c02b57c7b96a1fae
pramodshashindu39-netizen <pramodshashindu39@gmail.com>
2026-09-15 12:34:04 +0530

    switch case statements

--- js/app.js
+++ js/app.js
@@ -144,3 +144,37 @@ if(units <= 50){
 
 console.log("Your bill is: " + bill);
 
+//==================================================================
+//switch case statements
+//==================================================================
+
+console.log("\n\n");
+
+let day = 8
+
+switch(day){
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thurrsday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+    default:
+        console.log("Innvalid Day");
+        break
+}
