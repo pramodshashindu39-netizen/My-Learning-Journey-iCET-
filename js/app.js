3d4fb1f076de72cb95af6412e4ef18b0893cbf09
pramodshashindu39-netizen <pramodshashindu39@gmail.com>
2026-09-15 12:26:53 +0530

    if else statements

--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
 console.log("Hii Again ");
 console.log("This is my first External JavaScript");
 
+//==================================================================
 //Variables 
+//==================================================================
+
+console.log("\n\n");
+
 let name = "Shasindu Pramod";
 console.log(name);
 console.log(typeof name);
@@ -16,8 +21,12 @@ console.log(typeof studentAge);
 console.log(typeof studentPass);
 console.log(typeof student);
 
-//========================================================
+//==================================================================
 //Arithmatic Operators
+//==================================================================
+
+console.log("\n\n");
+
 let x=10;
 let y=8;
 let z=x*y;
@@ -31,10 +40,14 @@ let a="Shasindu";
 console.log(a*x);
 console.log(a+x);
 
-//========================================================
+//==================================================================
 //Comparison Operators
 
 //== (value ), === (type & value ), !=, !== (type and value ), >, <, >=, <=
+
+//==================================================================
+
+console.log("\n\n");
 
 let num1=10;
 let num2=20;    
@@ -56,6 +69,9 @@ console.log(num3!=num2);
 //========================================================
 //Logical Operators
 // && (AND), || (OR), ! (NOT)
+//========================================================
+
+console.log("\n\n");
 
 let isRaining = true;
 let isSunny = false;
@@ -63,6 +79,12 @@ let isSunny = false;
 console.log(isRaining && isSunny); // false
 console.log(isRaining || isSunny); // true
 console.log(!isRaining); // false
+
+//========================================================
+//if else statements
+//========================================================
+
+console.log("\n\n");
 
 if (true && false) {
     console.log("Both conditions are true");
@@ -80,5 +102,45 @@ if (true && true) {
     console.log("Both conditions are false");   
 }   
 
+// Grade Assignment
+console.log("\n\n");
 
+let marks = 65;
+
+if (marks >= 75) {
+    console.log("Grade: A");
+}else if(marks >=60){
+    console.log("Grade: B");
+}else if(marks >=50){
+    console.log("Grade: C");
+}else if(marks >=30){
+    console.log("Grade: S");
+}else{
+    console.log("Not Grade , Fail");
+}
+
+console.log("\n\n");
+
+let age = 13;
+
+if(age > 18){
+    console.log("You can access this website");
+}else{
+    console.log("You cannot access this website");   
+}
+
+console.log("\n\n");
+
+let units= 20;
+let bill=0;
+
+if(units <= 50){
+    bill = units * 10;
+}else if(units <= 100){
+    bill = units * 15;
+}else{
+    bill = units * 20;
+}
+
+console.log("Your bill is: " + bill);
 
